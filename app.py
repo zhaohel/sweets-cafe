f156a4d786f01c5e20b3acdc8a437f422cc39e5f
Helena <zhaohel@oregonstate.edu>
2026-03-05 15:52:33 -0800

    fixed syntax error

--- app.py
+++ app.py
@@ -43,4 +43,4 @@ if __name__ == "__main__":
 #prodction 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port), debug=True
+    app.run(host="0.0.0.0", port=port, debug=True)
